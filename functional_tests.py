809f04df0259889cb657b083b1ddabf9ef8e6757
schampoux <sergiochampoux@gmail.com>
2025-03-08 20:34:00 -0500

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 import unittest 
+from selenium.webdriver.common.keys import Keys 
+import time
+from selenium.webdriver.common.by import By
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -11,20 +14,36 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User hears about the online to-do list and decides to check out the home page  
         self.browser.get('http://localhost:8000')
-
+        
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+        self.assertIn('To-Do', header.txt)
 
         # She is invited to enter a to-do item straight away 
-
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 
+            'Enter a to-do item'
+        )
+        
         # She types "go to the store" into a text box
+        inputbox.send_keys('go to the store')
 
         # She hits enter, the page updates, and now the page lists 
         ## "1: Go to the store" as the first item in a to-do list 
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table=self.browser.find_element(By.ID,'id_list_table')
+        rows=table.find_elements(By.TAG_NAME,'tr')
+        self.assertTrue(
+                any(row.text == '1: go to the store' for row in rows)
+        )
 
         # The text box still exists, so she uses it again. 
         ## She enters, "grab apples from the fruit section"
+        self.fail('finish the test')
 
         # The page updates again and shows both items on her list 
 
