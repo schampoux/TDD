c4c45cff74c3a5cd9091cf0e5c54dd31cafdd908
schampoux <sergiochampoux@gmail.com>
2025-03-19 16:47:09 -0400

    Adjust the home page view so that all 'item' objects are rendered in the return statemt

--- lists/views.py
+++ lists/views.py
@@ -6,5 +6,8 @@ def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
+    
+    items = Item.objects.all()
 
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'items': items})
+
