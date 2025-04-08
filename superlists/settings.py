827743282ffeddc4146c18b1563573de39521ff4
schampoux <sergiochampoux@gmail.com>
2025-04-08 16:25:15 -0400

    add django_rich to installed apps for rich terminal output, assign TEST_RUNNER

--- superlists/settings.py
+++ superlists/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEST_RUNNER = 'django_rich.test.RichRunner'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'django_rich',
 ]
 
 MIDDLEWARE = [
