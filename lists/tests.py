33b8ef11c4d76c86509017370c5aeb48c86c7ae2
schampoux <sergiochampoux@gmail.com>
2025-04-10 18:17:16 -0400

    adjust our item model test to now handle lists. create the list object and assign it to the .list attribute of each item that enters a list. Create assertions: is the list saved, does the first items .list attribute match the object we created, same for the second one.

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List 
 from rich.traceback import install
 
 install()
@@ -30,30 +30,41 @@ class ListViewTest(TestCase):
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
 
-
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List() 
+        list_.save()
+
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "Item the second"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        
+        self.assertEqual(
+            first = saved_list, 
+            second = list_ 
+        )
 
         saved_items = Item.objects.all()
         
         self.assertEqual(
             first = saved_items.count(), 
             second = 2
-            )
+        )
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
@@ -61,13 +72,23 @@ class ItemModelTest(TestCase):
         self.assertEqual(
             first = first_saved_item.text, 
             second = "The first (ever) list item"
-            )
+        )
+        
+        self.assertEqual(
+            first = first_saved_item.list, 
+            second = list_ 
+        )
         
         self.assertEqual(
             first = second_saved_item.text, 
             second = "Item the second"
-            )
+        )
 
+        self.assertEqual(
+            first = second_saved_item.list,
+            second = list_
+        )
+        
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
         
