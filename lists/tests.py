9d9efd60a1ab6014fe29ba38f0c1f254a8b8f639
schampoux <sergiochampoux@gmail.com>
2025-04-17 12:49:03 -0400

    tests for adding an item to an existing list.

--- lists/tests.py
+++ lists/tests.py
@@ -131,3 +131,29 @@ class NewListTest(TestCase):
             response = response, 
             expected_url = f'/lists/{new_list.id}/'
             )
+
+class NewItemTest(TestCase):
+    def test_can_save_a_post_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create() 
+        
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data = {'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
