848af8ccade5fc875160071492d6c6202469751e
schampoux <sergiochampoux@gmail.com>
2025-04-17 11:48:52 -0400

    docstring for new arg. dont pass new are to render the HTML yet, we havent updated that.

--- lists/views.py
+++ lists/views.py
@@ -7,9 +7,12 @@ def home_page(request):
     return render(request, "home.html")
 
 
-def view_list(request):
-
-    items = Item.objects.all()
+def view_list(request, list_id):
+    """
+    list_id: str captured from the url identifying a specific list. 
+    """
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
     return render(request, "list.html", {"items": items})
 
 def new_list(request):
